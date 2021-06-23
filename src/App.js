b2d97e5451f06bd8c276a11e1c9fab8f7ffa4857
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2021-06-23 23:57:40 +0530

    component structure, styling menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 class App extends React.Component {
   render() {
