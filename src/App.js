0ece7a775c0b345f978d310c40c10f4ba70e2349
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2021-06-24 16:22:55 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
