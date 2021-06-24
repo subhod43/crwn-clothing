2d4dba9e83364378f88e9919b4f21e7c8703f5f2
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2021-06-24 12:50:18 +0530

    added 1st routing to hats page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
       </div>
     );
   }
