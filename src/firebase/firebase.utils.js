3ab06b490f38778aa58cb9cfcfcb757af9bfa539
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2021-06-27 18:58:11 +0530

    added signIn and signUp for firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -21,4 +21,27 @@ const config = {
   provider.setCustomParameters({prompt: 'select_account'});
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+  export const createUserProfile = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt
+        })
+      } catch(error) {
+        console.log('error creating user :', error.message);
+      }
+    }
+    return userRef;
+  }
+
   export default firebase;
